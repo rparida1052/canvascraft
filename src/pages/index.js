7d261f8ae4a5e4259b22423746a631ad6a586de5
Rahul Parida <rparida1052@gmail.com>
2023-10-25 19:48:20 +0530

    board_added

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Menu from '@/components/Menu'
 import ToolBox from '@/components/Toolbox'
+import Board from '@/components/Board'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,6 +12,7 @@ export default function Home() {
   <>
   <Menu/>
   <ToolBox/>
+  <Board/>
   </>
 
   )
